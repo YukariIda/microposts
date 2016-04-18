fff6f37c2f8526aac40947c0b4eac644e582c0ff
yukariida <yukari.ida@blowish.jp>
2016-04-18 14:53:30 +0000

    user-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  
+
+  before_action :collect_user, only: [:edit, :update] 
+
   def show # 追加
    @user = User.find(params[:id])
   end
@@ -17,11 +19,31 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to(root_url) if !current_user?(user) 
+    else
+      render 'edit'
+    end
+  end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :region)
   end
+
+  def collect_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) if @user != current_user
+  end
+
 end
